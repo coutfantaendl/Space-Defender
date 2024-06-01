eb733fe07bfecbfa9cc5d5eae4a65360006d9ce0
coutfantaendl <vlad_zaytsev_200@mail.ru>
2024-06-01 08:46:55 +0300

    Health
    Added a health system for the player

---
+++ Space-Defender/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+    [SerializeField] private int maxHealth;
+
+    [SerializeField] private SpriteRenderer[] healthIcons;
+
+    [SerializeField] private Sprite fullHeart;
+    [SerializeField] private Sprite emptyHeart;
+
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void UpdateHealthUI()
+    {
+        for(int i = 0; i < healthIcons.Length; i++)
+        {
+            if(i < currentHealth)
+            {
+                healthIcons[i].sprite = fullHeart;
+            }
+            else
+            {
+                healthIcons[i].sprite = emptyHeart;
+            }
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player Died");
+    }
+
+}
